7c985fdaae7fbdc294ee591efde955694314fe10
Vv <sharma.vivekaditya@gmail.com>
2026-08-23 20:43:39 +0530

    Lots for F&O, leveraged exposure for MTF

--- api/universe.js
+++ api/universe.js
@@ -1,7 +1,7 @@
-import { UNIVERSE, FNO, MTF3 } from "./_universe.js";
+import { UNIVERSE, FNO, MTF3, LOTS } from "./_universe.js";
 
 export default function handler(req, res) {
   res.setHeader("Access-Control-Allow-Origin", "*");
   res.setHeader("Cache-Control", "s-maxage=3600, stale-while-revalidate=86400");
-  return res.status(200).json({ universe: UNIVERSE, fno: FNO, mtf3: MTF3, count: UNIVERSE.length });
+  return res.status(200).json({ universe: UNIVERSE, fno: FNO, mtf3: MTF3, lots: LOTS, count: UNIVERSE.length });
 }
